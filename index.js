3f89980886f5028d325ca721074cf415bee6cbe1
Leah <elloworldt@gmail.com>
2021-12-01 12:55:12 -0800

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction(callbackFunction){
+return callbackFunction();
+}
+
+function returnsANamedFunction(number){
+return Math.round
+}
+
+    let returnsAnAnonymousFunction  = () => console.log('Anonymous function');
